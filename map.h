752a322d7bb4e0fdb33dafb55ee18d9c11d43bba
xiyanxiyan10@gmail.com <xiyanxiyan10@gmail.com>
2014-03-07 23:20:10 +0800

    map lib init

---
+++ map.h
@@ -0,0 +1,27 @@
+#ifndef  MAP_H_
+#define  MAP_H_
+/**
+ * @brief STL map 
+ * @file  map.h
+ * @author mhw
+ * @date 2014/3/11
+ * @note rb-tree depend
+ */
+
+#include "stdio.h"
+#include "string.h"
+#include "list.h"
+#include "rb_tree.h"
+
+typedef signed int      s_int32_t;
+typedef signed short    s_int16_t;
+typedef signed char     s_int8_t;
+
+typedef unsigned int    u_int32_t;
+typedef unsigned short  u_int16_t;
+typedef unsigned char   u_int8_t;
+
+
+
+
+#endif
