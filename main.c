3314a881c76efd34264e9bc7f5d014e0fa8b148e
xiyanxiyan10@gmail.com <xiyanxiyan10@gmail.com>
2014-03-08 06:07:32 +0800

    1.mem 相关重构完成

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
-#include "map.h"
+//#include "map.h"
+
+#include "rfc_mem.h"
 
 int main()
 {
+    struct rfc_mem *mem;
+    mem = alloc_rfc_mem(malloc, free, malloc, free);
+    if(!mem){
+        fprintf(stderr, "alloc mem fail\n");
+    }
+    destory_rfc_mem(mem);
     return 0;
 }
--- rfc_mem.h
+++ rfc_mem.h
@@ -4,48 +4,22 @@
  * @brief rfc内存管理
  * @file rfc_mem.h
  * @author mhw
- * @date 2014/3/10
+ * @date 2014/3/11
  *
  */
 #include "rfc_conf.h"
 
 /*mem manager for rfc*/
 struct rfc_mem{
-    void * (*talloc)(u_int32_t);                         /**<tmp mem alloc*/
-    void   (*tdestory)(void *);                             /**<tmp mem free*/
+    void * (*talloc)(u_int32_t);                    /**<tmp mem alloc*/
+    void   (*tdestory)(void *);                     /**<tmp mem free*/
 
-    void * (*palloc)(u_int32_t);                   /**<permanent mem alloc*/
-    void   (*pdestory)(void *);                       /**<permanent mem free*/
+    void * (*palloc)(u_int32_t);                    /**<permanent mem alloc*/
+    void   (*pdestory)(void *);                     /**<permanent mem free*/
 };
 
-/**
- * @brief init rfc mem manager
- * @brief talloc tmp memory alloc ptr 
- * @brief tdestory  tmp memory free  ptr 
- * @brief palloc  permanent memory alloc  ptr 
- * @brief pdestory   permanent memory free   ptr 
- * @return rfc_mem
- */
-static inline struct rfc_mem * init_rfc_mem(void *talloc(u_int32_t), void tdestory(void *), void *palloc(u_int32_t), void pdestory(void *)){
-    struct rfc_mem *mem_ret = (struct rfc_mem*)palloc(sizeof(struct rfc_mem));
-    if(!mem_ret)
-        return 0;
-    memset(mem_ret, 0 , sizeof(struct rfc_mem));
-    mem_ret->talloc       = talloc;
-    mem_ret->tdestory     = tdestory;
-    mem_ret->palloc = palloc;
-    mem_ret->pdestory  = pdestory;
-    return mem_ret;
-}
 
-/**
- * @brief deinit rfc mem manager
- * @param[in] rfc_mem_param rfc_mem wait to destory
- * @return 
- */
-static inline s_int32_t deinit_rfc_mem(struct rfc_mem *mem_ptr){
-    mem_ptr->pdestory(mem_ptr);
-    return 0;
-}
+struct rfc_mem * alloc_rfc_mem(void *talloc(u_int32_t), void tdestory(void *), void *palloc(u_int32_t), void pdestory(void *));
+void destory_rfc_mem(struct rfc_mem *mem_ptr);
 
 #endif
